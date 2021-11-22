c3a32871ca41e65b85d2327e9e46002f7a5b98ea
Dibrary <kkdh828@hanmail.net>
2021-11-23 08:55:56 +0900

    21-11-23 algorithm practice

--- writing_algorithm/6.07 third_big_number_2.py
+++ writing_algorithm/6.07 third_big_number_2.py
@@ -1,15 +1,15 @@
 '''
 3번째로 큰 수를 반환하자.
 입력 값이 3개보다 작다면, 큰 수를 반환하자.
-입력 값에는 중복이 없다.
+입력 값에는 중복이 있을 수 있다. 중복이 있다면, 갯수와 관계 없이 크기로만 3번째 값을 반환해보자.
 '''
 
 def test():
     print("input array")
     nums = [int(x) for x in input().split()]
-    nums.sort()
+    num_list = list(set(nums))
 
-    return nums[-3] if len(nums) > 3 else max(nums)
+    return num_list[-3] if len(num_list) > 3 else max(num_list)
 
 if __name__=="__main__":
     print(test())

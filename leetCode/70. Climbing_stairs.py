f8ebf092cec15742b8cda25dc03878cd1483efe7
Dibrary <kkdh828@hanmail.net>
2021-11-25 08:57:32 +0900

    21-11-25 시간 초과 걸린 코드 메모이제이션으로 수정

--- leetCode/70. Climbing_stairs.py
+++ leetCode/70. Climbing_stairs.py
@@ -5,14 +5,24 @@
 '''
 
 class Solution:
+    def __init__(self):
+        self.tmp = [False]*60
+
     def climbStairs(self, n: int) -> int:
         if n == 0:
             return 1
-        if n < 0:
+        elif n < 0:
             return 0
+
+        if self.tmp[n] == False:
+            self.tmp[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+        else:
+            return self.tmp[n]
         return self.climbStairs(n-1) + self.climbStairs(n-2)
+
+
 
 if __name__=="__main__":
     k = Solution()
-    print(k.climbStairs(40)) # 40만 가도 시간이 너무 오래 걸려서 통과가 안 됨.
+    print(k.climbStairs(40)) # 메모이제이션 안 쓰면 40만 가도 시간이 너무 오래 걸려서 통과가 안 됨.
 

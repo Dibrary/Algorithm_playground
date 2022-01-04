2eb04f26d3eb70941d9f77b07975096d23e3444c
Dibrary <kkdh828@hanmail.net>
2022-01-05 08:35:01 +0900

    22-01-05 algorithm practice

---
+++ BOJ/5543.py
@@ -0,0 +1,10 @@
+
+a = int(input())
+b = int(input())
+c = int(input())
+burger = min([a,b,c])
+
+coke = int(input())
+chilsung = int(input())
+drink = min([coke, chilsung])
+print("%d"%(burger+drink-50))

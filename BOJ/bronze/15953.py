98b1e1774dd3e09b7acbe9d1f2576d7e16c1ecbb
Dibrary <kkdh828@hanmail.net>
2022-01-10 22:01:33 +0900

    22-01-10 algorithm practice

---
+++ BOJ/bronze/15953.py
@@ -0,0 +1,20 @@
+
+first = [0]+[500]+[300]*2+[200]*3+[50]*4+[30]*5+[10]*6
+second = [0]+[512]+[256]*2+[128]*4+[64]*8+[32]*16
+
+n = int(input())
+for _ in range(n):
+    a, b = map(int, input().split())
+    if a > len(first):
+        a = 0
+    else:
+        a = first[a]
+    if b > len(second):
+        b = 0
+    else:
+        b = second[b]
+    print("%d"%((a+b)*10000))
+
+
+
+

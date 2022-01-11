7252dd83c49ad95d41d82defc01ae9e4fcfd3dcf
Dibrary <kkdh828@hanmail.net>
2022-01-11 10:31:50 +0900

    22-01-11 algorithm practice

--- BOJ/silver/1439.py
+++ BOJ/silver/1439.py
@@ -1,12 +1,57 @@
-s = input()
-zero_cnt = 0
-one_cnt = 0
-for i in range(0, len(s)-1):
-    print(s[i], s[i+1], "값")
-    if s[i] != s[i+1] and s[i] == 1:
-        one_cnt += 1
-    elif s[i] != s[i+1] and s[i] == 0:
-        zero_cnt += 1
-print(zero_cnt, one_cnt)
+# s = input()
+# zero_cnt = 0
+# one_cnt = 0
+# for i in range(0, len(s)-1):
+#     print(s[i], s[i+1], "값")
+#     if s[i] != s[i+1] and s[i] == 1:
+#         one_cnt += 1
+#     elif s[i] != s[i+1] and s[i] == 0:
+#         zero_cnt += 1
+# print(zero_cnt, one_cnt)
+
+# import sys
+# s = sys.stdin.readline() # 이 코드도 17%정도에서 틀림.
+#
+# if "0" not in s: # 아예 처음부터 1만 있으면 바꿀 필요가 없다.
+#     print(0)
+# else:
+#     if s[-1] =="0": s+= "1"
+#     else:           s+= "0"
+#     start = 0
+#     one_cnt, zero_cnt = 0, 0
+#
+#     for i in range(1, len(s)):
+#         if s[i] != s[i-1]:
+#             if "1" in s[start:i]:
+#                 one_cnt += 1
+#             else:
+#                 zero_cnt += 1
+#             start = i
+#     print(one_cnt, zero_cnt)
+#     if min(one_cnt, zero_cnt) == 0:
+#         print(1)
+#     else:
+#         print("%d"%(min(one_cnt, zero_cnt)))
+
+import sys
+
+s = sys.stdin.readline()
+
+if "1" not in s:
+    print(0)
+elif "0" not in s:
+    print(0)
+else:
+    if s[-1] == "0": s += "0"
+    else:            s += "1"
+
+    zero_cnt, one_cnt = 0,0
+    for i in range(len(s)-1):
+        if s[i]!=s[i+1]:
+            if s[i] == "1":   one_cnt += 1
+            elif s[i] == "0": zero_cnt += 1 # 이 부분 그냥 else로 빼면 에러남.
+    print("%d"%(min(one_cnt, zero_cnt)))
+
+
 
 
--- BOJ/silver/1789.py
+++ BOJ/silver/1789.py
@@ -4,5 +4,23 @@ total = 0
 for i in range(1, s):
     total += i
     if total > s:
-        print(i-1)
+        print(i-1) # 이렇게 하니 곧바로 틀림.
         break
+
+for i in range(1, 1000000):
+    value = ((i*(i+1))/2)
+    if value >= s:
+        print(i-1)
+        break # 이렇게 하니 막판에 가서 틀림.
+
+for i in range(1, 1000000):
+    value = ((i * (i + 1)) / 2)
+    if value > s:
+        print(i - 1)
+        break
+    elif value == s:
+        print(i)
+        break
+
+
+

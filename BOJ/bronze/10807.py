aabc25ef3019a645fc2ea2816f91b24a8c9e9c06
Dibrary <kkdh828@hanmail.net>
2022-01-20 08:25:56 +0900

    22-01-20 algorithm practice

---
+++ BOJ/bronze/10807.py
@@ -0,0 +1,7 @@
+
+n = int(input())
+arr = list(map(int, input().split()))
+value = int(input())
+print("%d"%(arr.count(value)))
+
+
--- BOJ/practice.py
+++ BOJ/practice.py
@@ -1,30 +1,13 @@
-import sys
 
-def searching(DDUK, target, start, end):
-    if start > end:
-        return False
+n = int(input())
 
-    mid = (start + end)//2
+ball = 1
 
-    tmp = 0
-    for i in DDUK:
-        if i < mid:
-            continue
-        else:
-            tmp += (i - mid)
-    print(tmp)
-
-    if tmp == target:
-        return mid
-    elif tmp < target:
-        return searching(DDUK, target, start, mid-1)
-    else:
-        return searching(DDUK, target, mid+1, end)
-
-
-n, target = list(map(int, input().split()))
-DDUK = list(map(int, sys.stdin.readline().split()))
-
-min_value, max_value = min(DDUK), max(DDUK)
-print(searching(DDUK, target, min_value, max_value))
-
+for i in range(n):
+    x, y = map(int, input().split())
+    if x == ball or y == ball:
+        if x == ball:
+            ball = y
+        elif y == ball:
+            ball = x
+print(ball)

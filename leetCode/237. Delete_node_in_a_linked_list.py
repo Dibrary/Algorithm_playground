59242e7dad3b15f47b90e54253c34af469e6ae08
Dibrary <kkdh828@hanmail.net>
2022-02-26 09:45:42 +0900

    22-02-26 leetcode algorithm practice

--- leetCode/237. Delete_node_in_a_linked_list.py
+++ leetCode/237. Delete_node_in_a_linked_list.py
@@ -16,3 +16,9 @@ def deleteNode(node):
             head = head.next
 
 # 이 문제는 list가 전달인자에 없다 =_=;;
+
+def deleteNode(node):
+    node.val = node.next.val
+    node.next = node.next.next
+
+# =_= 그냥 이게 답이란다...

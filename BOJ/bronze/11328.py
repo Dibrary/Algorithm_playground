e51f0b9e946b1c9e83d0d2e0ce5c026e78a303da
Dibrary <kkdh828@hanmail.net>
2022-04-01 08:41:57 +0900

    22-04-01 algorithm practice

--- BOJ/bronze/11328.py
+++ BOJ/bronze/11328.py
@@ -1,17 +1,31 @@
+# n = int(input())
+# for _ in range(n):
+#     a, b = map(str, input().split())
+#     a = list(a)
+#     b = list(b)
+#
+#     flag = 0
+#     for k in a:
+#         if k in b:
+#             b.remove(k)
+#     print(b)
+#     if b == []:
+#         print("Possible")
+#     else:
+#         print("Impossible")
+
+# 25%에서 틀렸다고 나옴.
+
 n = int(input())
 for _ in range(n):
     a, b = map(str, input().split())
     a = list(a)
     b = list(b)
 
-    flag = 0
-    for k in a:
-        if k in b:
-            b.remove(k)
-    print(b)
-    if b == []:
+    a.sort()
+    b.sort()
+
+    if a == b:
         print("Possible")
     else:
         print("Impossible")
-
-# 25%에서 틀렸다고 나옴.
---
+++ BOJ/gold/2418.py
@@ -0,0 +1,11 @@
+
+h, w, l = map(int, input().split())
+
+word_map = []
+for _ in range(h):
+    word_map.append(list(input()))
+
+# 헐 문제는 8방향으로 다 갈 수 있음 =_=
+
+
+

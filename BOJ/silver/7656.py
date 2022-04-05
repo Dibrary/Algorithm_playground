a352325abe4ede10cd5bc314111e6237208b3d5a
Dibrary <kkdh828@hanmail.net>
2022-04-05 09:06:39 +0900

    22-04-05 algorithm practice

---
+++ BOJ/silver/7656.py
@@ -0,0 +1,11 @@
+
+string = input().split()
+
+for i in string:
+    if "?" in i:
+        print(f'Forty-two is the answer to {i[:-1]}.')
+
+# 복수인 것이 있는 듯? what is the answer to A and to B? 뭐 이런꼴
+
+
+

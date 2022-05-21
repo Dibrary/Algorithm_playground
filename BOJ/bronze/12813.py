9f4a7de7ef081b7b749d928a5a72dc95900a77d3
KwangHyun <mikekhkim@gmail.com>
2022-05-21 18:11:26 +0900

    22-05-21 algorithm practice

--- BOJ/bronze/12813.py
+++ BOJ/bronze/12813.py
@@ -1,38 +1,53 @@
 
-def reverse_func(a, b):
-    a_reverse = ""
-    b_reverse = ""
+# def reverse_func(a, b):
+#     a_reverse = ""
+#     b_reverse = ""
+#
+#     for i in a:
+#         if i == "0":  a_reverse += "1"
+#         else:         a_reverse += "0"
+#
+#     for j in b:
+#         if j == "0":  b_reverse += "1"
+#         else:         b_reverse += "0"
+#
+#     return a_reverse[::-1], b_reverse[::-1] # 원본으로 반환
+#
+# def and_func(a, b): # 3개 함수 구현을 해야됨.
+#     return None
+#
+# def or_func(a, b):
+#     return None
+#
+# def xor_func(a, b):
+#     return None
 
-    for i in a:
-        if i == "0":  a_reverse += "1"
-        else:         a_reverse += "0"
-
-    for j in b:
-        if j == "0":  b_reverse += "1"
-        else:         b_reverse += "0"
-
-    return a_reverse[::-1], b_reverse[::-1] # 원본으로 반환
-
-def and_func(a, b): # 3개 함수 구현을 해야됨.
-    return None
-
-def or_func(a, b):
-    return None
-
-def xor_func(a, b):
-    return None
+def make_binary(n):
+    result = 0
+    for i in range(len(n)):
+        result += int(n[i])*(2**i)
+    return result
 
 a = input()[::-1]
 b = input()[::-1]
 
-result = []
-result.append(and_func(a, b))
-result.append(or_func(a, b))
-result.append(xor_func(a, b))
-a_reverse, b_reverse = reverse_func(a, b)
-result.append(a_reverse)
-result.append(b_reverse)
+a_bin = make_binary(a)
+b_bin = make_binary(b)
 
-for k in result:
-    print(k)
+# result = []
+# result.append(and_func(a, b))
+# result.append(or_func(a, b))
+# result.append(xor_func(a, b))
+# a_reverse, b_reverse = reverse_func(a, b)
+# result.append(a_reverse)
+# result.append(b_reverse)
+#
+# for k in result:
+#     print(k)
 
+print((bin(a_bin&b_bin)))
+print((bin(a_bin|b_bin)))
+print((bin(a_bin^b_bin)))
+
+print((bin(~a_bin))) # 이거 값이 이상
+print((bin(~b_bin))) # 이거 값이 이상

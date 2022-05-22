0d7a44bb3be05bd4ab6ca6c98db6dc16d41e3cd9
Dibrary <kkdh828@hanmail.net>
2022-05-22 10:59:13 +0900

    22-05-22 algorithm practice

--- BOJ/bronze/1193.py
+++ BOJ/bronze/1193.py
@@ -1,3 +1,39 @@
+#
+# table = [1]
+# for i in range(2, 400):
+#     table.append(table[-1]+i)
+#
+# n = int(input())
+#
+# index = 0
+# value = 0
+#
+# for idx, k in enumerate(table):
+#     if n <= k:
+#         index = idx
+#         value = k
+#         break
+#
+# cnt = n-table[index-1]-1
+#
+# if index%2 == 0:
+#     upper = index+1
+#     under = 1
+#     for k in range(cnt):
+#         upper -= 1
+#         under += 1
+# else:
+#     upper = 1
+#     under = index + 1
+#     for k in range(cnt-1, -1, -1):
+#         upper += 1
+#         under -= 1
+#
+# print("%d/%d"%(upper, under))
+
+# 지그재그로 이동할 때 x, y축에 해당되는 값 에서만 이상하게 나옴.
+
+# 위 코드 틀림
 
 table = [1]
 for i in range(2, 400):
@@ -14,15 +50,23 @@ for idx, k in enumerate(table):
         value = k
         break
 
-cnt = table[index] - n
-print(cnt, index, value)
+cnt = n-table[index-1]-1
 
-upper = 1 + cnt
-under = (index + 1) - cnt
+if index%2 == 0:
+    upper = index+1
+    under = 1
+    for k in range(cnt):
+        upper -= 1
+        under += 1
+else:
+    upper = 1
+    under = index + 1
+    for k in range(cnt-1, -1, -1):
+        upper += 1
+        under -= 1
 
 print("%d/%d"%(upper, under))
 
-# 지그재그로 이동할 때 x, y축에 해당되는 값 에서만 이상하게 나옴.
 
 
 
---
+++ BOJ/bronze/1212(입력 8진수로받기).py
@@ -0,0 +1,66 @@
+# import sys
+#
+# input = sys.stdin.readline
+#
+# n = input()
+# n = n[::-1]
+#
+# result = ""
+# for idx, i in enumerate(n):
+#     value = bin(int(i))[2:]
+#
+#     if len(value) == 1 and idx != len(n) - 1:
+#         value = "00" + value
+#     elif len(value) == 2 and idx != len(n) - 1:
+#         value = "0" + value
+#
+#     result = value + result
+# print(result)
+
+# 런타임 에러
+
+# import sys
+#
+# input = sys.stdin.readline
+#
+# n = input()
+# n = n[::-1]
+# n = n.replace("\n","")
+#
+# result = ""
+# for idx, i in enumerate(n):
+#     value = bin(int(i))[2:]
+#
+#     if len(value) == 1 and idx != len(n) - 1:
+#         value = "00" + value
+#     elif len(value) == 2 and idx != len(n) - 1:
+#         value = "0" + value
+#
+#     result = value + result
+# print(result)
+
+# 위 코드도 시간초과 걸린다.
+#import sys
+#input = sys.stdin.readline
+
+# table = {"0":"000","1":"001","2":"010","3":"011","4":"100","5":"101","6":"110","7":"111"}
+#
+# n = input()
+# n = n[::-1]
+# n = n.replace("\n","")
+#
+# result = ""
+# for i in n:
+#     result = table[i] + result
+# if result[:2] == "00":
+#     print(result[2:])
+# elif result[:2] == "01":
+#     print(result[1:])
+
+# 5% 갔다가 시간초과 걸림.
+
+
+# 헐 input 에서 진법으로 받는 방법이 있다.
+n = int(input(),8)
+print(bin(n)[2:])
+

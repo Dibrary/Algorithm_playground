bae0123b3e4363a54c1ead908e5010b16ace107b
Dibrary <kkdh828@hanmail.net>
2022-05-27 12:15:02 +0900

    22-05-27 algorithm practice

--- Programmers/신규아이디 추천.py
+++ Programmers/신규아이디 추천.py
@@ -1,105 +1,35 @@
-def double_spot_remove(s):  # 중복되는 점 모두 제거
-    tmp = ["." * x for x in range(2, len(s))]
-    tmp.sort(reverse=True)  # 역순 해야 긴 것 부터 적용됨.
 
-    for i in tmp:
-        s = s.replace(i, ".")
-
-    if len(s) == 1:
-        if s == ".":
-            return ""
-        else:
-            return s
-    else:
-        if s[0] == ".":
-            s = s[1:]
-        if s[-1] == ".":
-            s = s[:-1]
-
-    return s
-
-
-def particle_char_remove(s):
+def changer_lower(s):
     result = ""
     for i in s:
         if i.isalpha():
             result += i.lower()
-        elif i.isdigit():
-            result += i
-        elif i in ["-", "_", "."]:
+        else:
             result += i
     return result
 
+def replace_string(s):
+    result = ""
 
-def solution(new_id):
-    answer = None
+    for k in s:
+        if k.isdigit() or k.isalpha() or k == "_" or k == "-" or k == ".":
+            result += k
+    return result
 
-    new_id = particle_char_remove(new_id)
-    new_id = double_spot_remove(new_id)
+def replace_dot(s):
+    # table = ["."*n for n in range(1, len(s)) ]
+    # result = ""
+    # for i in table:
+    #     result = s.replace(i,".") # 이 코드의 문제는 1번만 적용된다는 것.
+    # return result
 
-    if len(new_id) > 15:
-        answer = new_id[:15]
-    elif 2 < len(new_id) < 16:
-        answer = new_id
-    else:
-        if len(new_id) < 3:
-            if len(new_id) == 0:
-                answer = "aaa"
-            else:
-                while len(new_id) < 3:
-                    new_id = new_id + new_id[-1]
-                answer = new_id
-    answer = double_spot_remove(answer)
-    return answer
-
-# 내 코드 통과
-
-
-### 다른 사람 코드 ###
-
-import re # 정규표현식 사용.
-
-def solution(new_id):
-    st = new_id
-    st = st.lower()
-    st = re.sub('[^a-z0-9\-_.]', '', st)
-    st = re.sub('\.+', '.', st)
-    st = re.sub('^[.]|[.]$', '', st)
-    st = 'a' if len(st) == 0 else st[:15]
-    st = re.sub('^[.]|[.]$', '', st)
-    st = st if len(st) > 2 else st + "".join([st[-1] for i in range(3-len(st))])
-    return st
+    pass
 
 
 
-### 다른 사람 코드 ###
-
-def solution(new_id):
-    answer = ''
-    # 1
-    new_id = new_id.lower()
-    # 2
-    for c in new_id:
-        if c.isalpha() or c.isdigit() or c in ['-', '_', '.']:
-            answer += c
-    # 3
-    while '..' in answer:
-        answer = answer.replace('..', '.')
-    # 4
-    if answer[0] == '.':
-        answer = answer[1:] if len(answer) > 1 else '.'
-    if answer[-1] == '.':
-        answer = answer[:-1]
-    # 5
-    if answer == '':
-        answer = 'a'
-    # 6
-    if len(answer) > 15:
-        answer = answer[:15]
-        if answer[-1] == '.':
-            answer = answer[:-1]
-    # 7
-    while len(answer) < 3:
-        answer += answer[-1]
-    return answer
+n = input()
+n = changer_lower(n)
+n = replace_string(n)
+n = replace_dot(n)
+print(n)
 

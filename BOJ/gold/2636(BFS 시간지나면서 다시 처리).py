747a829684f1577661664bfae5529fe14fa70bf8
Dibrary <kkdh828@hanmail.net>
2022-07-15 11:44:22 +0900

    22-07-15 algorithm practice

--- BOJ/gold/2636(BFS 시간지나면서 다시 처리).py
+++ BOJ/gold/2636(BFS 시간지나면서 다시 처리).py
@@ -56,3 +56,56 @@ for e in tmp:
 print("1회 전에 남아있는 치즈 값", count)
 
 
+
+
+### 다른 사람 코드 ###
+'''
+핵심은 치즈를 변경할 게 아니라 가장 밖에서 0인 것을 찾아 간 뒤에 해당 0과 접하는 1을 변경해버리는 것이다.
+'''
+
+from collections import deque
+
+r, c = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(r)]
+cheese = []
+
+dx = [0,0,-1,1]
+dy = [-1,1,0,0]
+
+def find_air():
+    visited = [[False]*c for _ in range(r)]
+
+    q = deque()
+    q.append([0,0])
+    visited[0][0] = True
+    cnt = 0
+
+    while q:
+        y, x= q.popleft()
+
+        for i in range(4):
+            ny = y + dy[i]
+            nx = x + dx[i]
+
+            if 0 <= ny < r and 0 <= nx < c and not visited[ny][nx]:
+                if board[ny][nx] == 0:
+                    visited[ny][nx] = True
+                    q.append([ny, nx])
+                elif board[ny][nx] == 1:
+                    board[ny][nx] = 0
+                    cnt += 1
+                    visited[ny][nx] = True
+    cheese.append(cnt)
+    return cnt
+
+time = 0
+while True:
+    time += 1
+    cnt = find_air()
+    if cnt == 0:
+        break
+
+print(time - 1)
+print(cheese[-2])
+
+

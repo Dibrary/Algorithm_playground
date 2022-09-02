7e0140118a12ab595201efacffcf64f6f269e8a0
Dibrary <kkdh828@hanmail.net>
2022-09-02 16:16:45 +0900

    22-09-02 algorithm practice

--- programmers/땅따먹기.py
+++ programmers/땅따먹기.py
@@ -17,6 +17,8 @@
 
 # 위 코드는 예제만 통과하지 모조리 실패함.
 
+
+
 # def solution(land):
 #     result = max(land[0])
 #     idx = land[0].index(result)
@@ -29,6 +31,7 @@
 
 # 위 코드도 모조리 실패함.
 # 반드시 첫 번째에서 max값으로 시작하는 게 답은 아닌듯?
+
 
 # def solution(land):
 #     result = []
@@ -45,7 +48,60 @@
 #     return max(result)
 
 # 위 코드도 모조리 실패.
-# 선택할 수 있는 것 중에 최대값만 선택하는 게 답이 아닌가?
+'''
+선택할 수 있는 것 중에 최대값만 선택하는 게 답이 아닌가?
+맞는데, 그 최대값이 중간에 튀어나오면 그걸 선택하는 것으로 바뀌어야 함.
+'''
+
+
+def solution(land):
+    temp = [0 for _ in range(len(land[0]))]
+
+    for index in range(1, len(land)):
+        result = [0 for _ in range(len(land[0]))]
+        if index == 1:
+            for idx, value in enumerate(land[index]):
+                result[idx] = (value + max(land[index - 1][:idx] + land[index - 1][idx + 1:]))
+        else:
+            for idx, value in enumerate(land[index]):
+                result[idx] = (value + max(temp[:idx] + temp[idx + 1:]))
+        temp = result
+    return max(temp)
+
+
+
+
+## 다른 사람 코드 ##
+
+def solution(land):
+
+    for i in range(1, len(land)):
+        for j in range(len(land[0])):
+            land[i][j] = max(land[i -1][: j] + land[i - 1][j + 1:]) + land[i][j]
+
+    return max(land[-1])
+
+
+
+
+
+
+
+
+
+
+import copy
+
+def hopscotch(board, size):
+    result = 0
+    # 땅따먹기 게임으로 얻을 수 있는 최대 점수는?
+    for i in range(1,size):
+        for j in range(4):
+            temp = copy.deepcopy(board[i-1])
+            temp[j] = 0
+            board[i][j]+=max(temp)
+    result = max(board[-1])
+    return result
 
 
 
@@ -57,6 +113,18 @@
 
 
 
+def solution(land):
+    n = len(land)
+
+    # dp[i][j] = i행 j열에서 점수의 최대값
+    dp = [[0,0,0,0]] + land
+    for i in range(1, n+1):
+        dp[i][0] += max(dp[i-1][1], dp[i-1][2], dp[i-1][3])
+        dp[i][1] += max(dp[i-1][0], dp[i-1][2], dp[i-1][3])
+        dp[i][2] += max(dp[i-1][0], dp[i-1][1], dp[i-1][3])
+        dp[i][3] += max(dp[i-1][0], dp[i-1][1], dp[i-1][2])
+
+    return max(dp[n])
 
 
 
@@ -68,9 +136,10 @@
 
 
 
-
-
-
-
-
-
+def hopscotch(board, size):
+    for i in range(size):
+        if i == size - 1:
+            return max(board[i])
+        for j in range(4):
+            # print j, board[i][:j] + board[i][j + 1:]
+            board[i + 1][j] += max(board[i][:j] + board[i][j + 1:])
